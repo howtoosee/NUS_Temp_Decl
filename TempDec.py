7e7ec6b884d26b6e64afddf3eda02a968c66bdba
Chen Xihao <chenxihao0627@gmail.com>
2021-08-06 14:44:58 +0800

    Update

--- TempDec.py
+++ TempDec.py
@@ -78,7 +78,7 @@ class TempDecl:
 
     def declTemp(self, am: bool = True):
         try:
-            self.driver.get("https://myaces.nus.edu.sg/htd")
+            self.driver.navigate().refresh()
 
             symptomsButton = WebDriverWait(self.driver, 120).until(
                 EC.presence_of_element_located((By.CSS_SELECTOR, "input[type='radio'][name='symptomsFlag'][value='N']"))
